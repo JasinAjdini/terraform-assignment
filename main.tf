f0e5bf3f1a8ba9e3dd6b1b8dd31cfd55fc733328
JasinAjdini <jasinajdini@gmail.com>
2023-03-30 21:09:54 +0200

    updated bucket acl

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "terrform-assignment-jasin-bucket-${var.environment}"
-  acl    = "private"
 
   tags = {
     Environment = var.environment
   }
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
 }
